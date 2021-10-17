3a3735645e0b9dd01bb4f7cb29aea895c9dfc425
Nilesh9492 <nilesh94041@gmail.com>
2021-10-17 17:31:28 +0530

    max float

--- Generic-Maximum/Program.cs
+++ Generic-Maximum/Program.cs
@@ -6,16 +6,22 @@ namespace Generic_Maximum
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter First Value :");
-            int FirstValue = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter First Float Value :");
+            double FirstValue = double.Parse(Console.ReadLine());
 
-            Console.WriteLine("Enter Second Value :");
-            int SecondValue = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Second Float Value :");
+            double SecondValue = double.Parse(Console.ReadLine());
 
-            Console.WriteLine("Enter Third Value :");
-            int ThirdValue = int.Parse(Console.ReadLine());
-            int maxInteger = MaximumInt(FirstValue,SecondValue,ThirdValue);
+            Console.WriteLine("Enter Third Float Value :");
+            double ThirdValue = double.Parse(Console.ReadLine());
+
+            int maxInteger = MaximumInt(13, 12, 48);
             Console.WriteLine("Maximum Integer : " + maxInteger);
+
+            double maxDouble = MaximumFloat(FirstValue,SecondValue,ThirdValue);
+            Console.WriteLine("Maximum Double  : " + maxDouble);
+
+
         }
         public static int MaximumInt(int FirstValue,int SecondValue,int ThirdValue)
         {
@@ -24,6 +30,16 @@ namespace Generic_Maximum
             if (SecondValue.CompareTo(FirstValue) > 0 && SecondValue.CompareTo(ThirdValue) > 0)
                 return SecondValue;
             if (ThirdValue.CompareTo(FirstValue) > 0 && ThirdValue.CompareTo(SecondValue) > 0)
+                return ThirdValue;
+            return 0;
+        }
+        public static double MaximumFloat(double FirstValue, double SecondValue, double ThirdValue)
+        {
+            if (FirstValue.CompareTo(SecondValue) > 0 && FirstValue.CompareTo(ThirdValue) > 0)
+                return FirstValue;
+            if (SecondValue.CompareTo(FirstValue) > 0 && SecondValue.CompareTo(ThirdValue) > 0)
+                return SecondValue;
+            if (ThirdValue.CompareTo(SecondValue) > 0 && ThirdValue.CompareTo(SecondValue) > 0)
                 return ThirdValue;
             return 0;
         }
