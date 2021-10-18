b29f0e1dc7f67f9ef0bcc3fd6594b1df62fa9962
Nilesh9492 <nilesh94041@gmail.com>
2021-10-18 09:40:47 +0530

    more than three parameters

---
+++ Generic-Maximum/GenericMax.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generic_Maximum
+{
+    class GenericMax<T> where T : IComparable
+    {
+        public T[] array;
+        public GenericMax(T[] array)
+        {
+            this.array = array;
+        }
+
+        public T[] SortValues(T[] array)
+        {
+            Array.Sort(array);
+            Array.Reverse(array);
+            return array;
+        }
+        public T GetMaximumValue()
+        {
+            var sortedValues = SortValues(this.array);
+            return sortedValues[0];
+        }
+    }
+}
--- Generic-Maximum/Program.cs
+++ Generic-Maximum/Program.cs
@@ -6,55 +6,12 @@ namespace Generic_Maximum
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter First Float Value :");
-            double FirstValue = double.Parse(Console.ReadLine());
+            int[] array = { 12,4,36,11,23,41,25 };
+            var maxVal = new GenericMax<int>(array).GetMaximumValue();
+            Console.WriteLine("Maximum value = " + maxVal);
 
-            Console.WriteLine("Enter Second Float Value :");
-            double SecondValue = double.Parse(Console.ReadLine());
-
-            Console.WriteLine("Enter Third Float Value :");
-            double ThirdValue = double.Parse(Console.ReadLine());
-
-            int maxInteger = MaximumInt(13, 12, 48);
-            Console.WriteLine("Maximum Integer : " + maxInteger);
-
-            double maxDouble = MaximumFloat(FirstValue,SecondValue,ThirdValue);
-            Console.WriteLine("Maximum Double  : " + maxDouble);
-
-            string maxString = MaximumString("OnePlus","Apple","Samsung");
-            Console.WriteLine("Maximum String Output : " + maxString);
-
-
+           
         }
-        public static int MaximumInt(int FirstValue,int SecondValue,int ThirdValue)
-        {
-            if (FirstValue.CompareTo(SecondValue) > 0 && FirstValue.CompareTo(ThirdValue) > 0)
-                return FirstValue;
-            if (SecondValue.CompareTo(FirstValue) > 0 && SecondValue.CompareTo(ThirdValue) > 0)
-                return SecondValue;
-            if (ThirdValue.CompareTo(FirstValue) > 0 && ThirdValue.CompareTo(SecondValue) > 0)
-                return ThirdValue;
-            return 0;
-        }
-        public static double MaximumFloat(double FirstValue, double SecondValue, double ThirdValue)
-        {
-            if (FirstValue.CompareTo(SecondValue) > 0 && FirstValue.CompareTo(ThirdValue) > 0)
-                return FirstValue;
-            if (SecondValue.CompareTo(FirstValue) > 0 && SecondValue.CompareTo(ThirdValue) > 0)
-                return SecondValue;
-            if (ThirdValue.CompareTo(SecondValue) > 0 && ThirdValue.CompareTo(SecondValue) > 0)
-                return ThirdValue;
-            return 0;
-        }
-        public static string MaximumString(string a, string b, string c)
-        {
-            if (a.CompareTo(b) > 0 && a.CompareTo(c) > 0)
-                return a;
-            if (b.CompareTo(a) > 0 && b.CompareTo(c) > 0)
-                return b;
-            if (c.CompareTo(a) > 0 && c.CompareTo(b) > 0)
-                return c;
-            return null;
-        }
+        
     }
 }
